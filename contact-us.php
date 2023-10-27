474200c4c0b38eae20facdad512d2b427af414f4
simonpetersonline@gmail.com <simonpetersonline@gmail.com>
2023-10-27 05:25:30 +0000

    sample php code added

---
+++ contact-us.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Check if the form has been submitted using POST method
+
+    if (isset($_POST["email"])) {
+        // Check if the "email" field has been set in the POST data
+
+        $email = $_POST["email"];
+        // Retrieve the value of the "email" field
+
+        // You can now process or use the $email variable as needed.
+        // For this example, we will simply display the email address.
+        echo "You have submitted the following email address: " . $email;
+    }
+}
+?>
